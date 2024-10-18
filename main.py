a38115e56a21ed57ec370fc188fda299480d4e29
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-10-17 21:04:13 -0700

    Returns avg result of all Models

--- main.py
+++ main.py
@@ -2,12 +2,39 @@ from fastapi import FastAPI
 import pickle
 import pandas as pd
 import uvicorn
+import numpy as np
 
-app = FastAPI()
+# Initialize FastAPI
+app = FastAPI()  
+    
+# Function to load model
+def load_model(filename):
+    with open(filename, "rb") as file:
+        return pickle.load(file)
 
-with open('xgb_model.pkl', 'rb') as file:
-    loaded_model = pickle.load(file)
+# Load Models
+xgboost_model = load_model('xgb_model.pkl')
 
+naive_bayes_model = load_model('nb_model.pkl')
+
+random_forest_model = load_model('rf_model.pkl')
+
+decision_tree_model = load_model('dt_model.pkl')
+
+extra_trees_model = load_model('et_model.pkl')
+
+svm_model = load_model('svm_model.pkl')
+
+knn_model = load_model('knn_model.pkl')
+
+voting_classifier_model = load_model('voting_clf.pkl')
+
+xgboost_SMOTE_model = load_model('xgboost-SMOTE.pkl')
+
+xgboost_featureEngineered_model = load_model('xgboost-featureEnginered.pkl')
+
+
+# Preprocess given data into a dataframe
 def preprocess_data(customer_dict):
     
     input_dict = {
@@ -31,17 +58,42 @@ def preprocess_data(customer_dict):
     
     customer_df = pd.DataFrame([input_dict])
     
-    print("customer_df")
-    print(customer_df)
-    
     return customer_df
 
+# Get predictions and probabilities from all models and return average
 def get_prediction(customer_dict):
     preprocessed_data = preprocess_data(customer_dict)
-    prediction = loaded_model.predict(preprocessed_data)
-    probability = loaded_model.predict_proba(preprocessed_data)
-    return prediction, probability
+    
+    probabilities = {
+        'XGBoost': xgboost_model.predict_proba(preprocessed_data)[0][1],
+        'Random Forest': random_forest_model.predict_proba(preprocessed_data)[0][1],
+        'K-Nearest Neighbors': knn_model.predict_proba(preprocessed_data)[0][1],
+        'Naive Bayes': naive_bayes_model.predict_proba(preprocessed_data)[0][1],
+        'Decision Tree': decision_tree_model.predict_proba(preprocessed_data)[0][1],
+        'Extra Trees': extra_trees_model.predict_proba(preprocessed_data)[0][1],
+        'SVM': svm_model.predict_proba(preprocessed_data)[0][1],
+        'Voting Classifier': voting_classifier_model.predict_proba(preprocessed_data)[0][1],
+        'XGBoost-SMOTE': xgboost_SMOTE_model.predict_proba(preprocessed_data)[0][1],
+        'XGBoost-FeatureEngineered': xgboost_featureEngineered_model.predict_proba(preprocessed_data)[0][1],
+    }
+    
+    predictions = {
+        'XGBoost': xgboost_model.predict(preprocessed_data)[0][1],
+        'Random Forest': random_forest_model.predict_proba(preprocessed_data)[0][1],
+        'K-Nearest Neighbors': knn_model.predict_proba(preprocessed_data)[0][1],
+        'Decision Tree': decision_tree_model.predict_proba(preprocessed_data)[0][1],
+        'Extra Trees': extra_trees_model.predict_proba(preprocessed_data)[0][1],
+        'SVM': svm_model.predict_proba(preprocessed_data)[0][1],
+        'Voting Classifier': voting_classifier_model.predict_proba(preprocessed_data)[0][1],
+        'XGBoost-SMOTE': xgboost_SMOTE_model.predict_proba(preprocessed_data)[0][1],
+        'XGBoost-FeatureEngineered': xgboost_featureEngineered_model.predict_proba(preprocessed_data)[0][1],
+    }
+    
+    avg_probability = np.mean(list(probabilities.values()))
+    avg_prediction = np.mean(list(predictions.values()))
+    return avg_prediction, avg_probability
 
+# Endpoint to get predictions and probabilities
 @app.post("/predict")
 async def predict(data: dict):
     
@@ -52,6 +104,7 @@ async def predict(data: dict):
         "probability": probabilities.tolist()
     }
 
+# Run the app
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=10000)
 
